85f7ba5321edaabfe854d52af1f51f53eb53aaf0
Ryan Smalley <flaxicon@gmail.com>
2015-10-06 20:17:43 -0400

    Jsoup ebay code
    Need to figure out how to have it include the jar for the external jsoup
    library. Grabs item id, title, and price

--- src/edu/mansfield/squirtle_squad/scraper/EbayScraper.java
+++ src/edu/mansfield/squirtle_squad/scraper/EbayScraper.java
@@ -1,50 +1,41 @@
 package edu.mansfield.squirtle_squad.scraper;
 
-import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.Validate;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import edu.mansfield.squirtle_squad.model.Item;
+import java.io.IOException;
 
-/*
- * 
- * Figured that it would be best passing the html into the class's
- * constructor and then all the data can be returned nicely. May
- * Require the an implementation of the data formatter to be 
- * functional. And you will definitely have to build the item class 
- * that is in the model package. If I think of anything else I will 
- * add it later. Remember to keep things generic.
- *
+/**
+ * Example program to list links from a URL.
  */
-public class EbayScraper extends Scraper{
-	
-	// String webpage is a variable of Scraper
-	
-	public EbayScraper(String webpage){
-		super(webpage);
-	}
-	
-	public int getItemCount(){
-		
-		/*
-		 *  TODO Return the number of items listed above the item listings
-		 *  
-		 *  This will be used to know whether the URL needs to be updated to
-		 *  narrow the search.
-		 *  
-		 */
-		webpage = "Blah";
-		return 0;
-	}
-	
-	public ArrayList<Item> getItemsListed(){
-		ArrayList<Item> itemsOnPage = new ArrayList<Item>(200);
-		
-		/*
-		 * TODO Return an ArrayList of the items listed on the webpage
-		 * 
-		 * Please keep the default size of the ArrayList at 200
-		 * 
-		 */
-		
-		return itemsOnPage;
-	}
+public class Ebay_Scraper {
+    public static void main(String[] args) throws IOException {
+        String url = "http://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_ipg=200&_nkw=all+items&rt=nc&LH_BIN=1";
+
+        Document doc = Jsoup.connect(url).get();
+
+        for(int i=0;i<=199;i++)
+        {
+    	String price = "" + doc.select("li.lvprice").get(i).text();
+
+    	System.out.println(i + " " + price);
+        }
+
+        for(int i=0;i<=199;i++)
+        {
+        String title = "" + doc.select("a.vip").get(i).text();
+        System.out.println(i + " " + title);
+        }
+
+        for(int i=0;i<=199;i++)
+        {
+        Element e = doc.select("li[listingid]").get(i);
+        String listingId = e.attr("listingid");
+        System.out.println(i + " " + listingId);
+        }
+
+    }
 }
