db21f5247ba955d5afb9a9d67fc3b4c3feea5694
Francis King <kingfg15@mounties.mansfield.edu>
2015-10-15 18:02:13 -0400

    Updated Database_Interactions class
    Added CRUD methods to the database handling class as well as a test to
    ensure the working functions of methods.

--- src/edu/mansfield/squirtle_squad/database/Database_Interactions.java
+++ src/edu/mansfield/squirtle_squad/database/Database_Interactions.java
@@ -1,5 +1,117 @@
 package edu.mansfield.squirtle_squad.database;
 
-public class Database_Interactions {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import edu.mansfield.squirtle_squad.model.Item;
+
+public class Database_Interactions {
+	public Connection dbConnect() {
+		Connection conn = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:Scraper.db");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		;
+		return conn;
+
+	}
+
+	public void makeTable(Connection conn) throws SQLException {
+		Statement stmt = null;
+		String sql = "CREATE TABLE EbayData\n" + "(id long NOT NULL,"
+				+ "title varchar(255) NOT NULL,"
+				+ "price DOUBLE PRECISION NOT NULL," + "bidTime long NOT NULL,"
+				+ "isAuction boolean);";
+		stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+		stmt.close();
+
+	}
+
+	public void dropTable(Connection conn) throws SQLException {
+		Statement stmt = null;
+		String sql = "DROP TABLE EbayData";
+		stmt = conn.createStatement();
+		stmt.execute(sql);
+		stmt.close();
+	}
+
+	public void insertData(Connection conn, Item item) throws SQLException {
+		Statement stmt = null;
+		stmt = conn.createStatement();
+		int boolValue = 0;
+		if (item.isAuction()) {
+			boolValue = 1;
+		}
+		String sql = "INSERT INTO EbayData (id, title, price, bidTime, isAuction)"
+				+ " VALUES ("
+				+ Long.toString(item.getId())
+				+ ", "
+				+ item.getTitle()
+				+ ", "
+				+ Double.toString(item.getPrice())
+				+ ", "
+				+ Long.toString(item.getBidTime())
+				+ ", "
+				+ Integer.toString(boolValue) + ");";
+		stmt.execute(sql);
+		stmt.close();
+	}
+
+	public Item[] getData(Connection conn) throws SQLException {
+		Item[] itemSet = null;
+		Statement stmt = null;
+		int results = 0;
+		stmt = conn.createStatement();
+		String sql = "SELECT COUNT (*) FROM EbayData;";
+		ResultSet ct = stmt.executeQuery(sql);
+		if (ct.next()) {
+			results = ct.getInt(1);
+		}
+		itemSet = new Item[results];
+		sql = "SELECT * FROM EbayData;";
+
+		ResultSet rs = stmt.executeQuery(sql);
+		if (rs != null) {
+			int i = 0;
+			while (rs.next()) {
+				long id = rs.getLong("id");
+				String title = rs.getString("title");
+				double price = rs.getDouble("price");
+				long bidTime = rs.getLong("bidTIme");
+				boolean isAuction = rs.getBoolean("isAuction");
+				Item item = new Item(id, title, price, bidTime, isAuction);
+				itemSet[i] = item;
+				i++;
+			}
+		}
+		stmt.close();
+		return itemSet;
+	}
+
+	public void deleteData(Connection conn, long deleteID) throws SQLException {
+		Statement stmt = null;
+		String sql = "DELETE FROM EbayData WHERE id =" + deleteID + ";";
+		stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+		stmt.close();
+	}
+
+	public void updateData(Connection conn, Item item, long replaceID)
+			throws SQLException {
+		Database_Interactions DI = new Database_Interactions();
+		DI.deleteData(conn, replaceID);
+		DI.insertData(conn, item);
+
+	}
 }
---
+++ src/edu/mansfield/squirtle_squad/tests/SQLiteTest.java
@@ -0,0 +1,42 @@
+package edu.mansfield.squirtle_squad.tests;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import edu.mansfield.squirtle_squad.database.Database_Interactions;
+import edu.mansfield.squirtle_squad.model.Item;
+
+public class SQLiteTest {
+
+	public static void main(String[] args) throws SQLException {
+		Item item1 = new Item(2914, "'Item Test'", 3.14, 1251, true);
+		Item item2 = new Item(291204, "'Item Test'", 3.4, 1232412351, true);
+		Item item3 = new Item(2912344, "'Item Test'", 1.6181, 12123491, false);
+		Item item4 = new Item(2914, "'Replacement Item'", .14, 9818694, true);
+		Database_Interactions DI = new Database_Interactions();
+		Connection conn = DI.dbConnect();
+		DI.dropTable(conn);
+		DI.makeTable(conn);
+		DI.insertData(conn, item1);
+		DI.insertData(conn, item2);
+		DI.insertData(conn, item3);
+		Item[] itemSet = DI.getData(conn);
+		for(int i = 0; i < itemSet.length; i++){
+			System.out.println(itemSet[i].toString() + "\n");
+		}
+		DI.deleteData(conn, 2914);
+		itemSet = DI.getData(conn);
+		System.out.println("After Delete:\n\n");
+		for(int i = 0; i < itemSet.length; i++){
+			System.out.println(itemSet[i].toString() + "\n");
+		}
+		DI.updateData(conn, item4, 291204);
+		itemSet = DI.getData(conn);
+		System.out.println("After Update:\n\n");
+		for(int i = 0; i < itemSet.length; i++){
+			System.out.println(itemSet[i].toString() + "\n");
+		}
+
+	}
+
+}
