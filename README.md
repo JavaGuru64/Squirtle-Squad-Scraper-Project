f5de1441303de03e74620630693917669fe10d1f
Francis King <kingfg15@mounties.mansfield.edu>
2015-10-19 14:06:35 -0400

    Update HelpGUI and README
    Made the HelpGUI read in the README file and it also now has a scroll
    pane to make reading it possible.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Squirtle-Squad-Scraper-Project
 This Project is a school project for a Systems Analysis class, The project requires our team to scrape data on the items listed on eBay and dealsea.com for price comparisons.
+
+HOW TO USE THIS APPLICATION:
+The use of this program is fairly straightforward. If you know that data already exists in the system then feel free to click on the ViewDB button to access the existing database.
+If you would like to update the information on the database then press the scan button and the program will scan ebay automatically. Currently this scanning process can take a long time. So please do this well in advance of needing to update. 
+To close the program click the Cancel button. 
+When using the DBGUI page you can search for a given title and have the data sorted by any of the selections available.
--- src/edu/mansfield/squirtle_squad/user_interface/HelpGUI.java
+++ src/edu/mansfield/squirtle_squad/user_interface/HelpGUI.java
@@ -2,10 +2,15 @@ package edu.mansfield.squirtle_squad.user_interface;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.JButton;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 
 public class HelpGUI {
@@ -39,15 +44,29 @@ public class HelpGUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		File readMe = new File("README.md");
+		String readMeText = "";
+		try {
+			Scanner sc = new Scanner(readMe);
+			while(sc.hasNext()){
+				readMeText += sc.nextLine() + "\n";
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		frmHelpMe = new JFrame();
 		frmHelpMe.setTitle("Help Me!");
 		frmHelpMe.setBounds(100, 100, 450, 340);
 		frmHelpMe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmHelpMe.getContentPane().setLayout(null);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 414, 240);
+		frmHelpMe.getContentPane().add(scrollPane);
+		
 		JTextPane textPane = new JTextPane();
-		textPane.setBounds(10, 11, 414, 240);
-		frmHelpMe.getContentPane().add(textPane);
+		scrollPane.setViewportView(textPane);
+		textPane.setText(readMeText);
 		
 		JButton btnOk = new JButton("Ok");
 		btnOk.addActionListener(new ActionListener() {
